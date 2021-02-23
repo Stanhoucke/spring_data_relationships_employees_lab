d23b648b502a78d6a368c9bcba6da7e3b7690e92
Stanhoucke <stanhoucke@gmail.com>
2021-02-23 10:22:34 +0000

    initial commit

---
+++ src/test/java/com/codeclan/example/employeerelationships/EmployeerelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeerelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeerelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
